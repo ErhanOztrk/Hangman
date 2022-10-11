9610e3a169335d0a09462993325cbb10245a5abb
Erhan <oztrkerhan@gmail.com>
2022-10-11 15:25:29 +0100

    created updateWrongLetters function

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const word_el = document.getElementById('word');
+const popup = document.getElementById('popup-container')
+const message_el = document.getElementById('succes-message')
+const wrongLetters_el = document.getElementById('wrong-letters')
+const items = document.querySelectorAll('.item')
 
-const correctletters = ['j','a'];
-const wrongletters = [];
+
+const correctLetters = [];
+const wrongLetters = [];
+const selectedWord = getRandomWord();
 
 // function that gets random words for the game
-getRandomWord = () => {
+function getRandomWord () {
     const words = ["javascript","java","phyton"];
     return words[Math.floor(Math.random()*words.length)]
 
@@ -14,17 +20,62 @@ getRandomWord = () => {
 
 // function to show random word to player
 function displayWord() {
-    const selectedWord = getRandomWord();
-
     word_el.innerHTML = `
     ${selectedWord.split('').map(letter => `
         <div class="letter">
-            ${correctletters.includes(letter) ? letter: ''}
+            ${correctLetters.includes(letter) ? letter: ''}
         </div>
     
         `).join('')}
     
     `;
+   
+   const w = word_el.innerText.replace(/\n/g,'')
+   if(w === selectedWord) {
+    popup.style.display = 'flex';
+    message_el.innerText = 'Congrulations'
+    //console.log('You Win!')
+   }
 }
+
+function updateWrongLetters() {
+    wrongLetters_el.innerHTML = `
+    ${wrongLetters.length > 0?'<h3>wrong letters</h3>':''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+
+items.forEach((item,index) => {
+    const errorCount = wrongLetters.length;
+
+    if(index<errorCount) {
+        item.style.display = 'block';
+    } else {
+        item.style.display = 'none';
+    }
+    })
+}
+
+window.addEventListener('keydown', function(e){
+    if(e.keyCode >= 65 && e.keyCode <= 90){
+    const letter = e.key;
+    //console.log(e.key)
+    //console.log(e.keyCode)
+
+    if (selectedWord.includes(letter)) {
+        if (!correctLetters.includes(letter)) {
+            correctLetters.push(letter);
+            displayWord();
+        } else {
+            console.log('this letter already selected')
+        }
+     }else  {
+        if(!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+         updateWrongLetters()
+     }
+     }
+    }
+})
 
 displayWord() 
